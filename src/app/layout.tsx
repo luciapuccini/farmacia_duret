9062e935a7f6a4400b4221b045d7118dbd689a2f
luciapuccini <puccinilucia@gmail.com>
2026-04-05 12:51:20 +0200

    clean up styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,43 @@
-import './globals.scss'
-import Navbar from '@/layout/navbar'
-import Footer from '@/layout/footer'
-import Container from '@/layout/container'
-import Breadcrumb from '@/layout/breadcrumb'
-import PageHeader from '@/layout/PageHeader'
+import "./reset.scss";
+import Breadcrumb from "@/layout/breadcrumb";
+import Container from "@/layout/container";
+import Footer from "@/layout/footer";
+import Navbar from "@/layout/navbar";
+import PageHeader from "@/layout/PageHeader";
 
 export const metadata = {
-  title: 'Farmacia Duret',
-  icons: { icon: '/favicon.ico' },
-}
+	title: "Farmacia Duret",
+	icons: { icon: "/favicon.ico" },
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="es">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body>
-        <Navbar />
-        <Container>
-          <PageHeader />
-          <Breadcrumb />
-          {children}
-        </Container>
-        <Footer />
-      </body>
-    </html>
-  )
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="es">
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body>
+				<Navbar />
+				<Container>
+					<PageHeader />
+					<Breadcrumb />
+					{children}
+				</Container>
+				<Footer />
+			</body>
+		</html>
+	);
 }
