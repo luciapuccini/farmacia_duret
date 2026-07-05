3d05d6585f62bbf3e75a6ad0715490f888ba0a12
luciapuccini <puccinilucia@gmail.com>
2026-07-05 12:04:22 +0200

    update rules

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig, devices } from '@playwright/test'
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './e2e',
-  use: { baseURL: 'http://localhost:3000' },
+  outputDir: 'playwright-report/raw',
+  // applies to every test’s browser context => goto(baseUrl)
+  use: { baseURL: 'http://localhost:4173' },
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
+    //builds and previews rather than running the dev server. Why: preview mode is closer to production
+    command: 'npm run preview',
+    url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI,
-    timeout: 60_000,
   },
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+  reporter: [
+    ['html', { open: 'never', outputFolder: 'playwright-report/html' }],
+    ['json', { outputFile: 'playwright-report/report.json' }],
+    ['list'],
   ],
-})
+  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+});
